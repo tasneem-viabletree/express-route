fcae2ae5fa4176a5ffa71f80e47b0af1a0eaac2e
mfaizyab <tasneem.faizyab@viabletree.com>
2022-12-29 19:34:49 +0500

    Update index.js

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ describe('Our application', function () {
 
     it('should understand basic mathematical principles', function () {
 
-        expect(5).to.equal(5);
+        expect(5).to.equal(7);
         expect(5).to.not.equal(3);
 
     });
